a71943446808ccca14a83d8b2fa7cac36e4c1d2a
thisisaaronland <devnull@localhost>
2019-12-14 13:35:50 -0800

    update vendor-deps

--- vendor/github.com/srwiley/rasterx/dash.go
+++ vendor/github.com/srwiley/rasterx/dash.go
@@ -1,12 +1,15 @@
 // Copyright 2017 by the rasterx Authors. All rights reserved.
 //_
 // created: 2017 by S.R.Wiley
+
 package rasterx
 
 import (
 	"golang.org/x/image/math/fixed"
 )
 
+// Dasher struct extends the Stroker and can draw
+// dashed lines with end capping
 type Dasher struct {
 	Stroker
 	Dashes                    []fixed.Int26_6
@@ -26,6 +29,7 @@ func (r *Dasher) joinF() {
 	}
 }
 
+// Start starts a dashed line
 func (r *Dasher) Start(a fixed.Point26_6) {
 	// Advance dashPlace to the dashOffset start point and set deltaDash
 	if len(r.Dashes) > 0 {
@@ -48,10 +52,6 @@ func (r *Dasher) Start(a fixed.Point26_6) {
 // lineF overides stroker lineF to modify the the call as below
 // while performing the join in a dashed stroke.
 func (r *Dasher) lineF(b fixed.Point26_6) {
-	if len(r.Dashes) == 0 {
-		r.Stroker.lineF(b)
-		return
-	}
 	var bnorm fixed.Point26_6
 	a := r.a // Copy local a since r.a is going to change during stroke operation
 	ba := b.Sub(a)
@@ -85,24 +85,34 @@ func (r *Dasher) lineF(b fixed.Point26_6) {
 // the dash pattern. Pass in nil or an empty slice for no dashes. dashoffset is the starting offset into the dash array.
 func (r *Dasher) SetStroke(width, miterLimit fixed.Int26_6, capL, capT CapFunc, gp GapFunc, jm JoinMode, dashes []float64, dashOffset float64) {
 	r.Stroker.SetStroke(width, miterLimit, capL, capT, gp, jm)
-	if len(dashes) > 0 { // Dashed Stroke
-		// Convert the float dash array and offset to fixed point and attach to the Filler
-		if len(dashes) > len(r.Dashes) {
-			r.Dashes = make([]fixed.Int26_6, len(dashes))
-		} else {
-			r.Dashes = r.Dashes[:len(dashes)]
-		}
-		for i, v := range dashes {
-			r.Dashes[i] = fixed.Int26_6(v * 64)
-		}
-		r.DashOffset = fixed.Int26_6(dashOffset * 64)
-		r.sgm = r
-	} else {
-		r.Dashes = r.Dashes[:0] // clear the dash array
-		r.sgm = &r.Stroker      // This is just plain stroking
+
+	r.Dashes = r.Dashes[:0] // clear the dash array
+	if len(dashes) == 0 {
+		r.sgm = &r.Stroker // This is just plain stroking
+		return
 	}
+	// Dashed Stroke
+	// Convert the float dash array and offset to fixed point and attach to the Filler
+	oneIsPos := false // Check to see if at least one dash is > 0
+	for _, v := range dashes {
+		fv := fixed.Int26_6(v * 64)
+		if fv <= 0 { // Negatives are considered 0s.
+			fv = 0
+		} else {
+			oneIsPos = true
+		}
+		r.Dashes = append(r.Dashes, fv)
+	}
+	if oneIsPos == false {
+		r.Dashes = r.Dashes[:0]
+		r.sgm = &r.Stroker // This is just plain stroking
+		return
+	}
+	r.DashOffset = fixed.Int26_6(dashOffset * 64)
+	r.sgm = r // Use the full dasher
 }
 
+//Stop terminates a dashed line
 func (r *Dasher) Stop(isClosed bool) {
 	if len(r.Dashes) == 0 {
 		r.Stroker.Stop(isClosed)
@@ -170,7 +180,7 @@ func (r *Dasher) QuadBezier(b, c fixed.Point26_6) {
 	r.quadBezierf(r.sgm, b, c)
 }
 
-// StrokeQuadBezier starts a stroked quadratic bezier.
+// CubeBezier starts a stroked cubic bezier.
 // It is a low level function exposed for the purposes of callbacks
 // and debugging.
 func (r *Dasher) CubeBezier(b, c, d fixed.Point26_6) {
@@ -183,11 +193,7 @@ func (r *Dasher) CubeBezier(b, c, d fixed.Point26_6) {
 // values.
 func NewDasher(width, height int, scanner Scanner) *Dasher {
 	r := new(Dasher)
-	if scanner != nil {
-		r.Scanner = scanner
-	} else {
-		r.Scanner = new(ScannerGV)
-	}
+	r.Scanner = scanner
 	r.SetBounds(width, height)
 	r.SetWinding(true)
 	r.SetStroke(1*64, 4*64, ButtCap, nil, FlatGap, MiterClip, nil, 0)
--- vendor/github.com/whosonfirst/go-whosonfirst-svg/README.md
+++ vendor/github.com/whosonfirst/go-whosonfirst-svg/README.md
@@ -4,14 +4,39 @@ Tools for working with Who's On First and SVG documents.
 
 ## Install
 
-You will need to have both `Go` (specifically a version of Go more recent than 1.7 so let's just assume you need [Go 1.9](https://golang.org/dl/) or higher) and the `make` programs installed on your computer. Assuming you do just type:
+You will need to have both `Go` (specifically a version [1.12](https://golang.org/dl/) or higher) and the `make` programs installed on your computer. Assuming you do just type:
 
 ```
-make bin
+make tools
 ```
 
 All of this package's dependencies are bundled with the code in the `vendor` directory.
 
+## Tools
+
+### wof2svg
+
+For example:
+
+```
+go run -mod vendor cmd/wof2svg/main.go sfo.geojson sfo.svg
+```
+
+Will produce:
+
+![](docs/images/sfo.png)
+
+And:
+
+```
+go run -mod vendor cmd/wof2svg/main.go --mercator sfo.geojson sfo-merc.svg
+```
+
+Will produce:
+
+![](docs/images/sfo-mercator.png)
+
+
 ## See also
 
-* https://github.com/fapian/geojson2svg
+* https://github.com/whosonfirst/go-geojson-svg
